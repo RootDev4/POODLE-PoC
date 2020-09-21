96bf5355d8a4a456e864ed8c71f4a864a55948e4
Chris <rootdev4@gmail.com>
2020-09-21 19:01:24 +0200

    Update httpserver.py

--- httpserver.py
+++ httpserver.py
@@ -33,7 +33,7 @@ def getHtml():
                 <p>
                     <button onclick="sendRequest()">Ping HTTPS Server</button>
                     <button onclick="findlengthblock()">Find CBC block length</button>
-                    <button onclick="sendAttack()">Run encryption</button>
+                    <button onclick="sendAttack()">Run decryption</button>
                 </p>
                 <script type="text/javascript">
                     const url = '""" + sys.argv[1] + """';
