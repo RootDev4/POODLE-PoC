2878249bb079fca0ac86b5bb58ecdc2b188b8f90
Chris <rootdev4@gmail.com>
2020-09-22 10:02:28 +0200

    Update recompile-openssl.sh

--- recompile-openssl.sh
+++ recompile-openssl.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Recompile current OpenSSL version with SSLv3.0 support.
+# @author: RootDev4 (c) 09/2020
+# @url: https://github.com/RootDev4/poodle-PoC
+
 echo "[>] Installing prerequisites"
 apt-get install make libtext-template-perl xutils-dev gcc build-essential checkinstall -y > /dev/null
 cd /usr/local/src
@@ -7,17 +11,17 @@ openssl_version=($(openssl version))
 echo "[>] Current OpenSSL version is ${openssl_version[1]}"
 
 echo "[>] Downloading source code of OpenSSL v${openssl_version[1]}"
-wget https://www.openssl.org/source/openssl-${openssl_version[1]}.tar.gz > /dev/null
+wget https://www.openssl.org/source/openssl-${openssl_version[1]}.tar.gz > /dev/null 2>&1
 tar -xf openssl-${openssl_version[1]}.tar.gz
 cd openssl-${openssl_version[1]}/
 chmod +x config
 
 echo "[>] Configure source code with SSLv3.0 support"
-./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared enable-ssl3 enable-ssl3-method > /dev/null
+./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared enable-ssl3 enable-ssl3-method > /dev/null 2>&1
 
 echo "[>] Start compiling. This may take a while..."
-make > /dev/null
-make install > /dev/null
+make > /dev/null 2>&1
+make install > /dev/null 2>&1
 
 echo "[>] Compiling finished. Creating necessary files and symlinks"
 cd /etc/ld.so.conf.d/
